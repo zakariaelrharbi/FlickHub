84c88b66c3ce3fda7260364d1943f01a807e65ea
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-11-21 17:10:08 +0100

    [FlickHub] redux installed

---
+++ FlickHub/src/assets/Components/HomeScreen.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../StyleCss/HomeScreen.css'
+
+const HomeScreen = () => {
+  return (
+    <div className='homescreen'>
+      {/* Navbar */}
+
+      {/* Banner */}
+
+      {/* Row */}
+      
+    </div>
+  )
+}
+
+export default HomeScreen
