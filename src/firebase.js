92d6facbf8d6a6cb6396b16212e364a6dd56d019
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-12-16 23:55:21 +0100

    firebase set up

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "flickhub-7b35b.firebaseapp.com",
+  projectId: "flickhub-7b35b",
+  storageBucket: "flickhub-7b35b.firebasestorage.app",
+  messagingSenderId: "293969060724",
+  appId: "1:293969060724:web:902ed6ad4e1bf7af150221"
+};
