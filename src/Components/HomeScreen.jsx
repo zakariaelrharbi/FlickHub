e92e34fe71b73b10fa5bbf97687820028a475d95
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-12-17 21:01:42 +0100

    adding homesreen

--- src/Components/HomeScreen.jsx
+++ src/Components/HomeScreen.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import '../css/HomeScreen.css'
 
 const HomeScreen = () => {
   return (
-    <div>
-      hi
+    <div className='HomeScreen'>
+      {/* Nav */}
+
+      {/* Banner */}
+      
+      {/* Rows */}
     </div>
   )
 }
