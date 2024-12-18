1f9990b151379d9c7768159ecebf60119de12152
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-12-18 21:18:43 +0100

    addding logo and avatar to navbar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import '../css/Nav.css'
+import Logo from '../assets/FLICKHUB.png'
+import Avatar from '../assets/avatar.jpg'
 
 const Nav = () => {
   return (
     <div className='Nav'>
+        {/* the logo */}
+          <img src={Logo} alt="" />
       This is the nav
+        {/* the avatar */}
+        <img src={Avatar} alt="" />
     </div>
   )
 }
