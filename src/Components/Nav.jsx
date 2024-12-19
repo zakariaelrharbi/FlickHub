5d84fe2b629659951f290f66b9fe888a3fa99ee8
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-12-19 18:43:39 +0100

    logo modified

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import '../css/Nav.css'
-import Logo from '../assets/FLICKHUB.png'
 import Avatar from '../assets/avatar.jpg'
 
 const Nav = () => {
   return (
-    <div className='Nav'>
+    <div className='Nav Nav_black'>
       <div className='Nav_contents'>
         {/* the logo */}
-          <img className='Nav_logo' src={Logo} alt="" />
+          <div className='Nav_logo'>FLICKHUB</div>
         {/* the avatar */}
         <img className='Nav_avatar' src={Avatar} alt="" />
       </div>
