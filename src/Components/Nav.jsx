cc36e1873fe2abba581b189856e1f1d5e257f643
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-12-19 19:24:48 +0100

    adding nav bar toggle to transparent when you scroll

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,11 +1,23 @@
-import React , {useState} from 'react'
+import React , {useState, useEffect} from 'react'
 import '../css/Nav.css'
 import Avatar from '../assets/avatar.jpg'
 
 const Nav = () => {
-  const [show, SetShow] = useState(0);
+  const [showNav, SetShowNav] = useState(0);
+  // function that hide and show nav bar while scrolling
+  const toggleNav = ()=>{
+    if(window.scrollY > 100){
+      SetShowNav(1);
+    }else{
+      SetShowNav(0);
+    }
+  }
+  useEffect(()=>{
+    window.addEventListener('scroll', toggleNav);
+    return () => window.removeEventListener('scroll', toggleNav)
+  }, [])
   return (
-    <div className='Nav Nav_black'>
+    <div className={` Nav ${showNav && 'Nav_black'}`}>
       <div className='Nav_contents'>
         {/* the logo */}
           <div className='Nav_logo'>FLICKHUB</div>
