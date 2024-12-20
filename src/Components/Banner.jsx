e6528a18aa52a84677dd5dacf553c4254478089c
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-12-20 20:14:04 +0100

    reseting css

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import '../css/Banner.css'
-import ImageBanner from '../assets/banner.webp'
 
 const Banner = () => {
   return (
     <header className='Banner'
     style={{
-      backgroundImage:{ImageBanner},
-      backgroundSize:'cover',
-      backgroundPosition:'center center',
+      backgroundSize:"cover",
+      backgroundImage:`url("https://cdn.playbackonline.ca/wp/wp-content/uploads/2020/05/Screen-Shot-2020-05-04-at-1.41.10-PM.png")`,
+      backgroundPosition:"center center",
     }}>
       
     </header>
