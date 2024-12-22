8a32d3d11cc9684f7bd413226d5c41ad672e03d6
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-12-22 15:22:06 +0100

    styling the rows done

--- src/Components/Rows.jsx
+++ src/Components/Rows.jsx
@@ -1,36 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import '../css/Rows.css'
+import React, { useEffect, useState } from 'react';
+import '../css/Rows.css';
 import axios from '../axios';
 
-
-const Rows = ({title, fetchUrl, isLargeRow = false}) => {
+const Rows = ({ title, fetchUrl, isLargeRow = false }) => {
   const [movies, setMovies] = useState([]);
+  const baseUrl = 'https://image.tmdb.org/t/p/original/';
 
-  const baseUrl = 'https://image.tmdb.org/t/p/original/'
-
-  useEffect(()=>{
+  useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(fetchUrl)
+      const request = await axios.get(fetchUrl);
       setMovies(request.data.results);
       return request;
-    }  
-    fetchData()
-  }, [fetchUrl])
+    }
+    fetchData();
+  }, [fetchUrl]);
+
   return (
-    <div className='Rows'>
+    <div className="Rows">
       <h2>{title}</h2>
-      {movies.map((movie)=>(
-        <img 
-        className={`Rows__poster ${isLargeRow && 'Rows__posterLarge'}`}
-        key={movie.id} 
-        src={`${baseUrl}${
-          isLargeRow ? movie.poster_path : movie.backdrop_path
-        }`} alt={movie.name} />
-      ))}
-      
+      <div className="Rows__posters">
+        {movies.map((movie) => {
+          // Ensure the movie has the required path before rendering
+          const imagePath = isLargeRow ? movie.poster_path : movie.backdrop_path;
+          if (!imagePath) return null;
 
+          return (
+            <img
+              key={movie.id}
+              className={`Rows__poster ${isLargeRow && 'Rows__posterLarge'}`}
+              src={`${baseUrl}${imagePath}`}
+              alt={movie.name || movie.title || movie.original_name}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Rows
+export default Rows;
--- src/Pages/HomeScreen.jsx
+++ src/Pages/HomeScreen.jsx
@@ -14,17 +14,17 @@ const HomeScreen = () => {
         <Banner/>
       {/* Rows */}
         <Rows 
-          title="NETFLIX ORIGINALS" 
+          title="FLICKHUB ORIGINALS" 
           fetchUrl={requests.fetchNetflixOriginals} 
           isLargeRow
         />
-        <Rows title="Trending Now" fetchUrl={requests.fetchTrending} />
-        <Rows title="Top Rated" fetchUrl={requests.fetchTopRated} />
-        <Rows title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-        <Rows title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-        <Rows title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-        <Rows title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-        <Rows title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+        <Rows isLargeRow title="Trending Now" fetchUrl={requests.fetchTrending} />
+        <Rows isLargeRow title="Top Rated" fetchUrl={requests.fetchTopRated} />
+        <Rows isLargeRow title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        <Rows isLargeRow title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Rows isLargeRow title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Rows isLargeRow title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Rows isLargeRow title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   )
 }
