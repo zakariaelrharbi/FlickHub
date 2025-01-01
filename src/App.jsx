d3462e24babb367b78b079da3b9f6129552aa5fa
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2025-01-01 19:51:43 +0100

    adding router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import HomeScreen from './Pages/HomeScreen'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <div className='App'>
-      <HomeScreen/>
+      <Router>
+        <Switch>
+          <Route path='/'><HomeScreen/></Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
