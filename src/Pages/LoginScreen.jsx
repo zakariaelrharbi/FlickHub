e4a7dfeed05e8a90fbe7762f1e9ad1649caeb628
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2025-01-02 19:56:04 +0100

    adding form

--- src/Pages/LoginScreen.jsx
+++ src/Pages/LoginScreen.jsx
@@ -13,6 +13,14 @@ const LoginScreen = () => {
       <div className="loginScreen_body">
         <>
             <h1>Unlimited movies, TV shows, and more.</h1>
+            <h2>Watch anywhere, Cancel at anytime</h2>
+            <h3>Ready to watch? Enter your email to create or restart your membership</h3>
+            <div className="loginScreen_input">
+              <form>
+                <input type='email' placeholder='Email Adress' />
+                <button className='loginScreen_Input_Button'>Get Started</button>
+              </form>
+            </div>
         </>
       </div>
     </div>
