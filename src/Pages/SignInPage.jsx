4e1b21b6bd530d359495c7ad094478b737dbdbf6
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2025-01-02 22:57:31 +0100

    styling form almost done

--- src/Pages/SignInPage.jsx
+++ src/Pages/SignInPage.jsx
@@ -6,9 +6,11 @@ const SignInPage = () => {
     <>
         <div className="Sign_InScreen">
             <form>
+                <h1>Sign In</h1>
                 <input type="email" placeholder='Your Email' />
                 <input type="password" placeholder='**********' />
                 <button type='submit'>Sign In</button>
+                <h4><span className='Sign_InScreen_gray'>New To Flickhub?</span> <span  className='Sign_InScreen_white'>Sign Up Now.</span></h4>
             </form>
 
         </div>
