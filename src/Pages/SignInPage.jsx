2c2ac75bb4973fe97a7cf1419d44c8c347baca9e
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2025-01-08 15:10:28 +0100

    fixing refresh when click on sign in button

--- src/Pages/SignInPage.jsx
+++ src/Pages/SignInPage.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import '../css/SignInPage.css'
 
 const SignInPage = () => {
+
+  const register = (e) => {
+    e.prenventDefault();
+  }
+  const login = (e) => {
+    e.prenventDefault();
+  }
+  
+
+
   return (
     <>
         <div className="Sign_InScreen">
@@ -9,7 +19,7 @@ const SignInPage = () => {
                 <h1>Sign In</h1>
                 <input type="email" placeholder='Your Email' />
                 <input type="password" placeholder='**********' />
-                <button type='submit'>Sign In</button>
+                <button type='submit' onClick={login}>Sign In</button>
                 <h4><span className='Sign_InScreen_gray'>New To Flickhub?</span> <span  className='Sign_InScreen_white'>Sign Up Now.</span></h4>
             </form>
 
