428d9425df3eddd7e2c4910ae458e55ca945c575
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2025-01-13 17:39:09 +0100

    create new user done

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import './App.css'
 import HomeScreen from './Pages/HomeScreen'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import LoginScreen from './Pages/LoginScreen';
+import { useEffect } from 'react';
+import { auth } from './firebase';
 
 function App() {
   const user = null;
 
+  useEffect(()=>{
+    const unsubscribe = auth.onAuthStateChanged((userAuth)=>{
+      if(userAuth){
+        console.log(userAuth)
+      }else{
+
+      }
+    })
+    return unsubscribe;
+  },[]);
   return (
     <div className='App'>
       {
