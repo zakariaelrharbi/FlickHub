e9d8e188b3ecf2c3fc07836867a8456deab41836
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2025-01-18 14:27:22 +0100

    import nav to profile screen

--- src/Pages/ProfileScreen.jsx
+++ src/Pages/ProfileScreen.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../css/ProfileScreen.css';
+import Nav from '../Components/Nav'
+
 
 const ProfileScreen = () => {
   return (
     <div className='profileScreen'>
-      this is the profile
+      <Nav/>
     </div>
   )
 }
